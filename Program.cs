a0a5c4455bbbd8c04d9c59e474a250baed8b6df2
Anatoly Luzgin <an.luzgin2012@yandex.ru>
2023-03-11 19:24:19 +0800

    Программа для задачи написана!

--- Program.cs
+++ Program.cs
@@ -13,22 +13,47 @@
 Console.Clear();
 
 // Ввод строки:
-string GetNumber(string messege)
+string GetNumber()
 {
-    string resulString = "Пустая строка!";
-    Console.Write("Введите текст!");
-    while(true)
-    {
-        if(int.TryParse(Console.Read(), out resulString))
-        {
-            break;
-        }
-        else
-        {
-            Console.Write("The string isn't entered! Enter a string: ");
-        }
-        return resulString;
-    }
+    string resulString = Console.ReadLine();
+    Console.WriteLine("");
+    return resulString;
+
 }
 
 
+string inputText = GetNumber();
+
+Console.WriteLine(inputText);
+
+// Метод селекции элементов текста
+void GetResult(string text)
+{
+    // Выделение одельных элементов;
+    // Записываем их в массив;
+    var data = text.Replace(",", "")
+                   .Split(" ")
+                   .ToArray();
+
+    // Находим число элементов в полученном массиве
+    int size = data.Length;
+
+    string lastText = String.Empty;
+    for(int i = 0; i < size; i++)
+    {
+        lastText = data[i];
+
+        // Выбираем элементы массива размером до 3 символов
+        if(data[i].Length <= 3)
+        {
+            var value = data[i];
+            Console.Write($"{value}, ");
+        }
+    }
+
+}
+
+Console.WriteLine("");
+
+// Вызываем метод:
+GetResult(inputText);
